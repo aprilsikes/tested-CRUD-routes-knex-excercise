f338ea3c78e8e0f287b857d790b803da99730a2a
april sikes <aprilsikes@gmail.com>
2016-01-19 15:19:11 -0700

    all tests-movies-routes passing

--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex')
+
+function Movies() {
+  return knex('movies');
+}
 
 /* Movies Routes Go Here. */
 
 router.get('/', function (req, res, next) {
   res.render('movies/index');
+})
+
+router.get('/new', function (req, res, next) {
+  res.render('movies/new');
+})
+
+router.post('/', function (req, res, next) {
+  res.redirect('/');
+})
+
+router.get('/:id', function (req, res, next) {
+  res.render('movies/show');
+})
+
+router.get('/:id/edit', function (req, res, next) {
+  res.render('movies/edit');
+})
+
+router.post('/:id', function (req, res, next) {
+  res.redirect('/movies/:id');
+})
+
+router.post('/:id/delete', function (req, res, next) {
+  res.redirect('/');
 })
 
 module.exports = router;
